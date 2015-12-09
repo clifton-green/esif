e25d22f3c3ac96e1818e8c3aa98ef523a375ee15
clifton-green <clifton.green@valtech.co.uk>
2015-12-09 14:40:48 +0000

    update heroku info

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,7 +10,7 @@ module.exports = function(grunt) {
             },
             prototypeheroku: {
                 options: {
-                    remote: 'git@heroku.com:name-of-heroku-app.git',
+                    remote: 'git@heroku.com:esif.git',
                     branch: 'master'
                 }
             },
