b9b9de0408be3c6a791172f0d68962bcebed5ee6
Henry Charge <henry.charge@valtech.co.uk>
2015-12-09 13:31:34 +0000

    Move site folders into sub-folder, added grunt task for build control

---
+++ Gruntfile.js
@@ -0,0 +1,37 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        buildcontrol: {
+            options: {
+                dir: 'prototype',
+                commit: true,
+                push: true,
+                message: 'Built %sourceName% from commit %sourceCommit% on branch %sourceBranch%'
+            },
+            prototypeheroku: {
+                options: {
+                    remote: 'git@heroku.com:henrys-new-site.git',
+                    branch: 'master'
+                }
+            },
+            local: {
+                options: {
+                remote: '../',
+                branch: 'build'
+                }
+            }
+        }
+
+    });
+
+    [
+        'grunt-contrib-connect',
+        'grunt-build-control'
+    ].forEach(function(task) {
+        grunt.loadNpmTasks(task);
+    });
+
+
+    grunt.registerTask('deploy', ['buildcontrol']);
+
+};
