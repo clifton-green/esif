8a8ac73e48e01bbb7f9223e4e13ea2dfa8f3ea01
Alex Ward <alex@volcaniccreations.com>
2016-02-19 17:18:10 +0000

    included node and npm engine details in package.json also added a postinstall script for heroku. modified server.js to point to /dist again and deleted the procfile as I don't think it's rquired anymore. Also updated .travis.yml to account for updated changes

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 var express = require('express');
 var app = express();
-app.use(express.static(__dirname + '/'));
+app.use(express.static(__dirname + '/dist'));
 
 app.listen(process.env.PORT || 5000);
